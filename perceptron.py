42cdb0b1baf8cf1052b813ea2d203efd1d1fdf2f
dixianjun <baonanhai@gmail.com>
2017-12-27 23:11:42 +0800

    添加注释

--- perceptron.py
+++ perceptron.py
@@ -39,10 +39,13 @@ class Perceptron(object):
 def plot_decision_regions(X, y, classifier, resolution=0.02):
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    cmap = ListedColormap(colors[:len(np.unique(y))])  # 生成颜色样板
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1  # 取样本一个属性的最大最小值
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1  # 取样本另外一个属性的最大最小值
+    # 生成点格数据
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    # 把点格数据拿出来做分类
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
